8623ac1beb84da017961180d044a8b37ee5c4adb
Lucas Gouveia Belon <lucasgouveiabelon@gmail.com>
2023-04-21 06:02:10 -0300

    first TODO done

--- InstallTermuxDesktop.sh
+++ InstallTermuxDesktop.sh
@@ -1,8 +1,15 @@
-Primeiro passo: Baixar o VNC Viewer.
-Segundo passo: Habilitar repositorio X11.
+<<Primeiro_passo
+Baixar o VNC Viewer.
+$ pkg install vncserver
+Primeiro_passo
+
+<<Segundo_passo : Habilitar repositorio X11.
 $ pkg install x11-repo
 (Para desabilitar uninstall x11-repo)
-Terceiro passo: 
+Segundo_passo
+
+<<Terceiro_passo 
+instalar e iniciar um servidor vnc
 $ pkg install tigervnc
 $ vncserver -localhost # Esse é o comando para deixar ligado o servidor.
 Criar senha e armazenar em lugar seguro.
@@ -19,7 +26,10 @@ Senha: 12345678
 Já tá lá o servidor, ativo o tempo todo (Depois eu mato ele)
 Agora vou precisar de um gerenciador de janelas e um ambiente grafico.
 Vou primeiro instalar um ambiente grafico.
+Terceiro_passo
 
+<<Quarto_passo
+Gerenciador de janelas.
 $ pkg install xfce4
 O arquivo ~/.vnc/xstartup deve conter apenas
 # !/data/data/com.termux/files/usr/bin/sh
@@ -30,4 +40,8 @@ netsurf, web browser simplão
 xfce4-terminal, emulador de terminal
 
 Nao foi preciso o gerenciador de janelas
+Quarto_passo
 
+pkg install vncserver x11-server tigervnc xfce4
+vncserver -localhost
+export DISPLAY=":1"
--- README.md
+++ README.md
@@ -12,3 +12,9 @@ Não se parece o tipo de coisa que se deve lembrar de cabeça, e é sempre prude
 andar com anotações por perto.
 A ideia é continuar procurando outros meios para aprender mais sobre o assunto. 
 É de fato muito interessante.
+
+Encontrei um livro, e também terminei de aprender uma nova linguagem de programação.
+Ahora esse repositório vai receber mais atenção. Além de que pretendo aprender e montar
+coisas bacanas por aqui. Quem sabe não crio uma interface pro gnuplot que eu consiga
+usar de forma adequada. O importante é se manter evoluindo. Eventualmente pretendo
+escrever um livro, e todas as ferramentas do gnuplot serão de muito grande ajuda.
